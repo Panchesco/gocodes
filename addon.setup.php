6a9bc69348d43e85f8d83f97f665467e75a4f49c
Richard Whitmer <richard@panchesco.com>
2016-12-14 13:47:50 -0700

    addon.setup.php
    Initial Commit: Addon setup file

---
+++ addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'author'      => 'Godat Design/Richard Whitmer',
+  'author_url'  => 'http://godatdesign.com',
+  'name'        => 'Good at PHP',
+  'description' => 'PHP for template tagdata',
+  'version'     => '1.0.0',
+  'namespace'   => 'Godat\Addons\Gdtphp',
+  'settings_exist' => FALSE,
+);
